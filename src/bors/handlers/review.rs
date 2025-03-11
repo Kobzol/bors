c2bf0398643375856b50a72760168bf911148751
Sakibul Islam <sakibulislam25800@gmail.com>
2025-03-11 15:07:44 +0000

    Add test for approve with sha

--- src/bors/handlers/review.rs
+++ src/bors/handlers/review.rs
@@ -859,4 +859,21 @@ approve = ["+approved"]
         })
         .await;
     }
+
+    #[sqlx::test]
+    async fn approve_with_sha(pool: sqlx::PgPool) {
+        run_test(pool, |mut tester| async {
+            tester.post_comment("@bors r+").await?;
+            tester.expect_comments(1).await;
+
+            let pr = tester.get_default_pr().await?;
+            assert_eq!(
+                pr.approved_sha,
+                Some(format!("pr-{}-sha", default_pr_number()))
+            );
+
+            Ok(tester)
+        })
+        .await;
+    }
 }
